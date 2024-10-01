1b9ec654525093f1793652cc7c0271638fae40fb
Austin <aayers575@gmail.com>
2024-10-01 14:53:59 -0500

    Book

---
+++ app/src/main/java/practice1/construcors/Student.java
@@ -0,0 +1,29 @@
+package practice1.construcors;
+
+public class Student {
+    private int studentId;
+    private String studentName;
+    private String grade;
+
+    public Student() {
+        this(0, "Unknown", "None");
+    }
+
+    public Student(int studentId, String studentName, String grade) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.grade = grade;
+    }
+
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        System.out.println("Student1 ID: " + student1.studentId);
+        System.out.println("Student1 Name: " + student1.studentName);
+        System.out.println("Student1 Grade: " + student1.grade);
+
+        Student student2 = new Student(101, "Cullen", "A");
+        System.out.println("Student2 ID: " + student2.studentId);
+        System.out.println("Student2 Name: " + student2.studentName);
+        System.out.println("Student2 Grade: " + student2.grade);
+    }
+}
